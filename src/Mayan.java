ccdb54fe77033966b99c7644414f3e2033a8d1bc
Hara <harachung90@gmail.com>
2022-07-30 22:14:00 +1000

    Second commit. Step 6. Created a .java file for Mayan class.

---
+++ src/Mayan.java
@@ -0,0 +1,6 @@
+public class Mayan extends Language {
+
+    public Mayan(String langName, int langNumSpeakers) {
+        super(langName, langNumSpeakers, "Central America", "verb-object-subject");
+    }
+}
